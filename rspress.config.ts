e14061219caf8f43ac2184d69d674c55151e0155
mikoto2000 <mikoto2000@gmail.com>
2026-02-01 19:35:05 +0900

    Google Analytics 導入。

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,5 +1,6 @@
 import * as path from 'node:path';
 import { defineConfig } from 'rspress/config';
+import { pluginGoogleAnalytics } from 'rsbuild-plugin-google-analytics';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
@@ -20,6 +21,10 @@ export default defineConfig({
         content: 'https://github.com/mikoto2000/Practical-Introduction-to-Spring-Boot',
       },
     ],
-    sidebar: false,
-  }
+  },
+  builderPlugins: [
+    pluginGoogleAnalytics({
+      id: 'G-RTY3TRXY5T',
+    }),
+  ],
 });
